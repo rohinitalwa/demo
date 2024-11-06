ac11df0330b8ce97fa38a58754034ca5b6ce49ea
Rohini talwade <rohinitalwade15@gmail.com>
2024-11-06 21:18:54 +0530

    adding heroservice

---
+++ src/services/hero/hero.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from '../../types/hero.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  apiUrl = 'https://66b76b1a7f7b1c6d8f1bd4ff.mockapi.io/hero/api/users';
+  constructor(private http: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.apiUrl);
+  }
+}
